c2a98d0a15745f1788d2f416963c931e5e861012
Vinayak Kumawat <iamvinayakkt@gmail.com>
2024-12-04 22:12:52 +0530

    fixing deploment issue try 3

--- src/app/api/users/[id]/route.ts
+++ src/app/api/users/[id]/route.ts
@@ -5,8 +5,8 @@ import path from 'path';
 const USERS_FILE = path.join(process.cwd(), 'data', 'users.csv');
 
 export async function DELETE(
-  request: Request,
-  { params }: { params: { id: string } }
+  _request: Request,
+  { params }: { params: { id: string }, searchParams: { [key: string]: string | string[] | undefined } }
 ) {
   try {
     const content = await fs.readFile(USERS_FILE, 'utf-8');
